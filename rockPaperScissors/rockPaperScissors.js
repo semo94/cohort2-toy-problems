4d289adfc8893a6e5d4194299e70cc7198c402dc
ruaa bani nameh <roaamohammad27@gmail.com>
2016-12-31 10:58:44 +0200

    initial commit

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,4 +1,15 @@
-
+function RPS(){
+	var result = [];
+	var rpsArr = ["rock","paper","scissors"];
+	for (var i = 0; i < rpsArr.length; i++) {
+		for (var j = 0; j < rpsArr.length; j++) {
+			for (var k = 0; k < rpsArr.length; k++) {
+				result.push([rpsArr[i],rpsArr[j],rpsArr[k]]);
+			}
+		}
+	}
+	return result;
+}                                                                                                                                                      
 /*
 * Write a function that generates every sequence of throws a single
 * player could throw over a three-round game of rock-paper-scissors.
