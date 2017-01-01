bd8069a775083fac8bb2182699a58c01bca9ac5c
semo94 <saleem.bakri94@gmail.com>
2017-01-01 04:44:59 +0200

    done with heap sort algorithm

--- angrams/allAnagrams.js
+++ angrams/allAnagrams.js
@@ -12,10 +12,31 @@
 /**
   * example usage:
   * var anagrams = allAnagrams('abc');
-  * console.log(anagrams); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+  * console.log(anagrams); // ['abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
 var allAnagrams = function(string) {
-	
+var array = string.split("");
+return heapsPermute(array);
+};
 
+var swap = function (array, pos1, pos2) {
+  var temp = array[pos1];
+  array[pos1] = array[pos2];
+  array[pos2] = temp;
+};
+
+var heapsPermute = function (array,result,n) {
+	result = result || [];
+  n = n || array.length; 
+  if (n === 1) {
+    result.push(array.join(""));
+  } else {
+    for (var i = 1; i <= n; i += 1) {
+      heapsPermute(array,result, n - 1);
+      var j = (n % 2) ? 1 : i;
+      swap(array, j - 1, n - 1); 
+    }
+  }
+  return result;
 };
