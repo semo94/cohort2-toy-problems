496dc3a0e6c58ebe733f8d835927813bf29cae5a
semo94 <saleem.bakri94@gmail.com>
2017-01-26 09:17:01 +0200

    solution

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -10,7 +10,21 @@
  * console.log(index); // 4
  */
 
-var binarySearch = function (array, target) {
-  
+var binarySearch = function (array, target, midPoint) {
+		var midPoint = midPoint || Math.floor(array.length/2);
+		if(array[midPoint] === target){
+			return midPoint;
+		}
+		else if(midPoint === 0 || midPoint === array.length-1){
+			return "not found";
+		}
+		else{
+			if(target > array[midPoint]){
+				midPoint = Math.ceil( ( midPoint + (array.length-1) ) /2 );
+			}else{
+				midPoint = Math.floor(midPoint/2);
+			}
+			return binarySearch(array,target,midPoint)
+		}
 };
 
